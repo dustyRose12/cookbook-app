0bcf0c865124cb03b9fd8f01813c27a3a58cdae6
dustyRose12 <dustyrose125@gmail.com>
2017-08-28 21:30:20 -0500

    cookbook-app 1st day 8.28

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+recipe = Recipe.new(
+                                  title: "Raw eggs",
+                                  chef: "Swedish Chef",
+                                  ingredients: "live chicken, pair of gloves",
+                                  directions: "put gloves on hand, firmly hold chicken, squeeze"
+                                  )
+
+recipe.save
+
+recipe = Recipe.new(
+                                  title: "Hand Sandwich",
+                                  chef: "Josh",
+                                  ingredients: "left hand, right hand, lettuce, tomato, mayo, cheese",
+                                  directions: "place ingredients on left hanf, put right hand on top, enjoy"
+                                  )
+
+recipe.save
+
+recipe = Recipe.new(
+                                  title: "Garbage Lagsana",
+                                  chef: "Bad Josh",
+                                  ingredients: "old chinese food, socks, banana peel, hot pepper",
+                                  directions: "layer ingredients in a pan, preheat over to 350, cook for 5 hours, let cool"
+                                  )
+
+recipe.save
+
+
