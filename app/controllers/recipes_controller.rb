3d1b6d809786e1ae05a5462444d574a0547ad88f
dustyRose12 <dustyrose125@gmail.com>
2017-08-31 21:27:05 -0500

    cookbook-app

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,35 @@
 class RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.first
-    render 'one_recipe_page.html.erb'
+  # def one_recipe_action
+  #   @recipe = Recipe.first
+  #   render 'one_recipe_page.html.erb'
+  # end
+
+  # def all_recipes_action
+  #   @recipes = Recipe.all
+  #   render 'all_recipes_page.html.erb'
+  # end
+
+  def index
+    @recipes = Recipe.all
   end
+
+  def new
+
+  end
+
+  def create
+    recipe = Recipe.new(
+                                    title: params[:title],
+                                    chef: params[:chef],
+                                    ingredients: params[:ingredients],
+                                    directions: params[:directions]
+                                    )
+    recipe.save
+  end
+
+  def show
+    #matches the /recipes/:id url param
+    @recipe = Recipe.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  get '/one_recipe_url'  => 'recipes#one_recipe_action'
+  # get '/one_recipe_url'  => 'recipes#one_recipe_action'
+  # get '/all_recipes_url' => 'recipes#all_recipes_action'
+
+  get '/' => 'recipes#index' #this makes a shortcut so you dont have to type recipes every time on the url.
+  get '/recipes' => 'recipes#index' #URL is recipes, action is index
+
+  get '/recipes/new' =>  'recipes#new' #always put new before :id, otherwise it thinks you're trying to pass new as an id
+  post '/recipes' =>  'recipes#create'
+
+  get '/recipes/:id' => 'recipes#show' #URL is recipes with ID, action is show to show a specific one
+
+
 end
